acec807f90e615b0bee7a899491a7c1b4b79b2fc
yildirim-murat <muratyildirim.work@gmail.com>
2024-05-26 14:37:11 +0300

    Dockerfile and docker-compose added

---
+++ src/test/java/gov/ankara112/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.ankara112.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
